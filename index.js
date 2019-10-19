1b65f0091688c70322d39ebce1a7806b2921d918
Roman Gurovich <romangurovich@gmail.com>
2019-10-19 16:32:22 -0700

    Organize the code a bit

--- index.js
+++ index.js
@@ -1,45 +1,62 @@
 var MidiWriter = require('midi-writer-js');
 
-var track = new MidiWriter.Track();
+function buildSong() {
+    var track = new MidiWriter.Track();
 
-track.addEvent([
-    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
-    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
-    new MidiWriter.NoteEvent({pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8'}),
-    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
-    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'})
-  ], function(event, index) {
-    return {sequential: true};
-  }
-);
+    track.addEvent([
+        new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+        new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
+        new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+        new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
+        new MidiWriter.NoteEvent({pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8'}),
+        new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+        new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'})
+      ], function(event, index) {
+        return {sequential: true};
+      }
+    );
 
-var write = new MidiWriter.Writer(track);
-var build = write.buildFile();
+    var write = new MidiWriter.Writer(track);
+    return write.buildFile();
+}
+
+function saveSongToFile(song) {
+    const fs = require('fs');
+
+    fs.writeFile('symphony.mid', song, (err) => {
+        // throws an error, you could also catch it here
+        if (err) throw err;
+
+        // success case, the file was saved
+        console.log('file saved!');
+    });
+}
+
+function publishSong() {
+    var song = buildSong();
+    saveSongToFile(song);
+}
 
 
-const fs = require('fs');
+function run() {
+    var inputs = [];
 
-// fs.writeFile('symphony.mid', build, (err) => {
-//     // throws an error, you could also catch it here
-//     if (err) throw err;
+    var lineReader = require('readline').createInterface({
+      input: require('fs').createReadStream('viirs.raw.ordered.csv')
+    });
 
-//     // success case, the file was saved
-//     console.log('file saved!');
-// });
+    lineReader.on('line', function (line) {
+        inputs.push(parseInt(line, 10))
+    });
 
-var inputs = [];
+    lineReader.on('close', function() {
+        publishSong();
+        console.log(inputs); // has its values now
+    });
+}
 
-var lineReader = require('readline').createInterface({
-  input: require('fs').createReadStream('viirs.raw.ordered.csv')
-});
+run();
 
-lineReader.on('line', function (line) {
-    inputs.push(parseInt(line, 10))
-});
 
-lineReader.on('close', function() {
-    console.log(inputs); // has its values now
-});
+
 
