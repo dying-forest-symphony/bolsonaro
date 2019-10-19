8cef16f9e4efadaf578a06e968afe3fd1bef6a38
Roman Gurovich <romangurovich@gmail.com>
2019-10-19 15:57:21 -0700

    A flicker

---
+++ index.js
@@ -0,0 +1,30 @@
+var MidiWriter = require('midi-writer-js');
+
+var track = new MidiWriter.Track();
+
+track.addEvent([
+    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
+    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'}),
+    new MidiWriter.NoteEvent({pitch: ['C4', 'C4', 'C4', 'C4', 'D4', 'D4', 'D4', 'D4'], duration: '8'}),
+    new MidiWriter.NoteEvent({pitch: ['E4','D4'], duration: '4'}),
+    new MidiWriter.NoteEvent({pitch: ['C4'], duration: '2'})
+  ], function(event, index) {
+    return {sequential: true};
+  }
+);
+
+var write = new MidiWriter.Writer(track);
+var build = write.buildFile();
+
+
+const fs = require('fs');
+
+fs.writeFile('symphony.mid', build, (err) => {
+    // throws an error, you could also catch it here
+    if (err) throw err;
+
+    // success case, the file was saved
+    console.log('file saved!');
+});
