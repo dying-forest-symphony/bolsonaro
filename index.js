1825f7bed6c5233f77a90e7dc93bceee1695d8c4
cswanson <cswanson@sysdel.com>
2019-10-19 22:58:01 -0700

    Added multi tracks.

--- index.js
+++ index.js
@@ -14,21 +14,80 @@ function velocity(point) {
     return v;
 }
 
+function velocityCymbal(point) {
+    p = parseInt(point, 10);
+
+    if (p > 5000) {
+        return 100;
+    }
+
+    return 0;
+}
+
+function velocityBlownBottle(point) {
+    p = parseInt(point, 10);
+
+    if (p == 0) {
+        return 50;
+    }
+
+    return 0;
+}
+
 function buildSong(fireData) {
+    var GUITAR = 49;
+    var CYMBAL = 32;
+    var BLOWN_BOTTLE = 73;
+
     var track = new MidiWriter.Track();
+    var guitarTrack = new MidiWriter.Track();
+    var cymbalTrack = new MidiWriter.Track();
+    var blownBottleTrack = new MidiWriter.Track();
     track.setTempo(90);
+    guitarTrack.setTempo(90);
+    cymbalTrack.setTempo(90);
+    blownBottleTrack.setTempo(90);
+
+    guitarTrack.addEvent(new MidiWriter.ProgramChangeEvent({instrument: GUITAR}));
+    cymbalTrack.addEvent(new MidiWriter.ProgramChangeEvent({instrument: CYMBAL}));
+    blownBottleTrack.addEvent(new MidiWriter.ProgramChangeEvent({instrument: BLOWN_BOTTLE}));
 
     for( var i = 0; i < fireData.length; i++) {
         track.addEvent([
-            new MidiWriter.NoteEvent({pitch: ['C4'], duration: '16',
-                velocity: velocity(fireData[i])}),
+            new MidiWriter.NoteEvent({pitch: ['C1'], duration: '16',
+                velocity: 0, channel: 1}),
           ], function(event, index) {
             return {sequential: true};
           }
         );
+
+        guitarTrack.addEvent([
+            new MidiWriter.NoteEvent({pitch: ['A1'], duration: '16',
+                velocity: velocity(fireData[i]), channel: 2}),
+          ], function(event, index) {
+            return {sequential: true};
+          }
+        );
+
+        cymbalTrack.addEvent([
+            new MidiWriter.NoteEvent({pitch: ['G8'], duration: '16',
+                velocity: velocityCymbal(fireData[i]), channel: 3}),
+          ], function(event, index) {
+            return {sequential: true};
+          }
+        );
+
+        blownBottleTrack.addEvent([
+            new MidiWriter.NoteEvent({pitch: ['E1'], duration: '16',
+                velocity: velocityBlownBottle(fireData[i]), channel: 4}),
+          ], function(event, index) {
+            return {sequential: false};
+          }
+        );
+
     }
 
-    var write = new MidiWriter.Writer(track);
+    var write = new MidiWriter.Writer([track, guitarTrack, cymbalTrack, blownBottleTrack]);
     return write.buildFile();
 }
 
